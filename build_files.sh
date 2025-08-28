170547483b3eeb5770ac6d573a9ea2bcddb3c951
Elishacker <mahendaelisha60@gmail.com>
2025-08-28 07:55:38 +0300

    inventory Updates

--- build_files.sh
+++ build_files.sh
@@ -1,14 +1,5 @@
+# build_files.sh
 #!/bin/bash
-
-echo "---- Running build_files.sh ----"
-
-# Install dependencies
 pip install -r requirements.txt
-
-# Run migrations
 python manage.py migrate --noinput
-
-# Collect static files
 python manage.py collectstatic --noinput --clear
-
-echo "---- Build complete ----"
